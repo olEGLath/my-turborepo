2f54d3212ee5037aa1566c3e266e58b6adb837a6
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-23 19:26:37 -0300

    Desenvolvendo o back-end: Rota: Autenticação com GitHub

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,7 @@ import { getProfile } from './routes/auth/get-profile'
 import { createAccount } from './routes/auth/create-account'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -49,6 +50,7 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
