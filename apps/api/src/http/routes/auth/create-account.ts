179977228291298d9b3c43b0242f440d16065fb9
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-23 15:52:47 -0300

    Desenvolvendo o back-end: Rota: Cadastro de usuário

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -15,7 +18,31 @@ export async function createAccount(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      return reply.status(201).send({ message: 'User created!' })
-    }
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'User with same e-mail already exists.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
+    },
   )
 }
