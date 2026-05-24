a5c92651d76b2b912692dbc8c4ea1f006f34d6b0
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-05-24 14:49:30 -0300

    Desenvolvendo o back-end: Rota: Encerrar organização

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -24,6 +24,7 @@ import { getMembership } from './routes/orgs/get-membership'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { getOrganization } from './routes/orgs/get-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -74,6 +75,7 @@ app.register(getMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
+app.register(shutdownOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🚀 HTTP server running on http://localhost:3333')
