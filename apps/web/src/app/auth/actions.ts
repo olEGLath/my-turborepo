d99f723fa8ad7cac7ea84702749bb25d883dd46a
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-06-01 17:46:46 -0300

    Desenvolvendo o front-end - Utilizando variáveis ambiente

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,12 +1,13 @@
 'use server'
 
+import { env } from "@saas/env"
 import { redirect } from "next/navigation";
 
 export async function signInWithGitHub() {
     const githubSignInUrl = new URL('login/oauth/authorize', 'https://github.com');
 
-    githubSignInUrl.searchParams.set('client_id', 'YOUR_CLIENT_ID');
-    githubSignInUrl.searchParams.set('redirect_uri', 'YOUR_REDIRECT_URI');
+    githubSignInUrl.searchParams.set('client_id', env.GITHUB_CLIENT_ID);
+    githubSignInUrl.searchParams.set('redirect_uri', env.GITHUB_REDIRECT_URI);
     githubSignInUrl.searchParams.set('scope', 'user');
 
     redirect(githubSignInUrl.toString());
--- packages/env/index.ts
+++ packages/env/index.ts
@@ -13,7 +13,9 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.string().url(),
   },
   client: {},
-  shared: {},
+  shared: {
+    API_URL: z.string().url()
+  },
   runtimeEnv: {
     SERVER_PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
@@ -22,6 +24,7 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
+    API_URL: process.env.API_URL,
   },
   emptyStringAsUndefined: true,
 })
