64c348c0fa518dd7138c26f7adfcd99394129804
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-06-02 19:09:48 -0300

    Desenvolvendo o front-end - Comportamento do Sheet

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,10 +1,11 @@
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 import { OrganizationForm } from '../../create-organization/organization-form'
 
 export default function CreateOrganization() {
     return (
         <Sheet defaultOpen>
-            <SheetContent>
+            <InterceptedSheetContent>
                 <SheetHeader>
                     <SheetTitle>Create Organization</SheetTitle>
                 </SheetHeader>
@@ -12,7 +13,7 @@ export default function CreateOrganization() {
                 <div className="py-4">
                     <OrganizationForm />
                 </div>
-            </SheetContent>
+            </InterceptedSheetContent>
         </Sheet>
     )
 }
