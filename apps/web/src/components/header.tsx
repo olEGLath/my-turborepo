6a8fbed55334deb072ffbd857ee458fe9f3bbf69
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-06-04 16:05:23 -0300

    Desenvolvendo o front-end - Navegação do app por abas

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 import { ability } from '@/auth/auth'
+
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 import { ProjectSwitcher } from './project-switcher'
@@ -13,7 +14,7 @@ export async function Header() {
     const permissions = await ability()
 
     return (
-        <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
+        <div className="mx-auto flex max-w-[1200px] items-center justify-between">
             <div className="flex items-center gap-3">
                 <Image src={rocketseatIcon} alt="Rocketseat" />
 
