8dfad4bfe3c8ce31bfe7cca8e03a78b35c38a7c6
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-06-05 00:06:54 -0300

    Desenvolvendo o front-end - Página: configurações da org

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,6 +1,6 @@
 import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
 import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
-import { OrganizationForm } from '../../create-organization/organization-form'
+import { OrganizationForm } from '../../org/organization-form'
 
 export default function CreateOrganization() {
     return (
